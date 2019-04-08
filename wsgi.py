a47586dc9e51e4e0f1b0134d23284b261163040f
bsinanovic <belmin.sinanovic@noris.de>
2019-04-08 14:12:24 +0200

    changed Greeting

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Gegrüßt seien die die sich auf diesem Planeten befinden der sich Erde nennt!"
 
 if __name__ == "__main__":
     application.run()
